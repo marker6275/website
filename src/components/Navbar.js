a0c9adec1ea92dfd9837c895e5c957619ce071d7
Mark Li <mkli627554@gmail.com>
2023-09-07 22:22:06 -0500

    cleaning up

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,40 +5,25 @@ export default function Navbar() {
 
     return (
         <div className="bg-[#282828] h-20 flex justify-between w-full font-inter">
-            <a href='/'>
+            <Link to="/">
                 <div className="bg-blue-500 w-20 h-full flex justify-center items-center text-white text-4xl">
                     <img src={logo} alt="Mark"/>
                 </div>
-            </a>
+            </Link>
+                
             <div className='flex'>
                 <Link to="/" className="flex p-3 justify-center items-center text-white w-40 hover:bg-[#424242] text-lg">
-                        HOME
+                    HOME
                 </Link>
-                {/* <a href='/' className="flex p-3 justify-center items-center text-white w-40 hover:bg-[#424242] text-lg">
-                    
-                </a> */}
                 <Link to="/music" className="flex p-3 justify-center items-center text-white w-40 hover:bg-[#424242] text-lg">
-                        MUSIC
+                    MUSIC
                 </Link>
-                {/* <a href='/music' className='flex p-3 justify-center items-center text-white w-40 hover:bg-[#424242] text-lg'>
-                    
-                </a> */}
                 <Link to="/contact" className="flex p-3 justify-center items-center text-white w-40 hover:bg-[#424242] text-lg">
-                CONTACT
+                    CONTACT
                 </Link>
-                {/* <a href='/contact' className='flex p-3 justify-center items-center text-white w-40 hover:bg-[#424242] text-lg'>
-                    <div>
-                        CONTACT
-                    </div>
-                </a> */}
                 <Link to="/future" className="flex p-3 justify-center items-center text-white w-40 hover:bg-[#424242] text-lg">
                     Other
                 </Link>
-                {/* <a href='/future' className='flex p-3 justify-center items-center text-white w-40 hover:bg-[#424242] text-lg'>
-                    <div>
-                        Other
-                    </div>
-                </a> */}
             </div>
         </div>
     )
