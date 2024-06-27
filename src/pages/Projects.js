5bd6cc5b9b4c4224fb1a8877830a786331ac74c8
Mark Li <mkli@Marks-Air.hsd1.il.comcast.net>
2024-06-26 22:33:41 -0500

    various formatting

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -9,7 +9,7 @@ function Projects() {
 
     return (
         <div className="bg-[#EBE8E2] h-screen flex flex-col items-center justify-center">
-            <h1 className="text-6xl font-semibold mt-6 mb-20">
+            <h1 className="text-6xl font-semibold mb-20">
                 My Projects
             </h1>
             <div className="grid grid-cols-2 flex justify-items-center w-full gap-y-20 p-10">
