057eccc7920cb12985d836323b7f2dd6537f9ca9
Mark Li <mkli627554@gmail.com>
2025-02-26 03:31:53 -0600

    update color

--- src/components/homepage/AboutMeComponent.js
+++ src/components/homepage/AboutMeComponent.js
@@ -2,7 +2,7 @@ import { RandomColorButton } from "../misc";
 
 export function AboutMe() {
   return (
-    <div className="flex justify-center items-center bg-[#eee] px-8 md:px-16 py-6 shadow-md rounded-lg mx-10 md:mx-20">
+    <div className="flex justify-center items-center bg-gradient-to-b from-white to-[#eee] uration-300 px-8 md:px-16 py-6 shadow-md rounded-lg mx-10 md:mx-20">
       <div>
         <div className="flex justify-center items-center">
           <h1 className="text-2xl py-4">About Me</h1>
