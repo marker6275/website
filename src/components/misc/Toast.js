08ef09c9788890e9cbc102b8b8d67b1e8d8b1e30
Mark Li <mkli@Marks-Air.hsd1.il.comcast.net>
2025-07-10 16:14:57 -0500

    fixed toast for small screens

--- src/components/misc/Toast.js
+++ src/components/misc/Toast.js
@@ -5,9 +5,9 @@ export class Toast extends React.Component {
   render() {
     const { isFadingOut } = this.props;
     return (
-      <div className={`fixed bottom-4 left-1/2 transform -translate-x-1/2 z-50 bg-[#fbf7f2] shadow-lg py-3 px-3 gap-3 rounded-md transition-all duration-300 ease-in-out ${isFadingOut ? 'opacity-0' : 'opacity-100 animate-fade-in'} flex justify-evenly`}>
+      <div className={`fixed bottom-4 left-1/2 transform -translate-x-1/2 z-50 bg-[#fbf7f2] shadow-lg py-3 px-3 gap-3 rounded-md transition-all duration-300 ease-in-out ${isFadingOut ? 'opacity-0' : 'opacity-100 animate-fade-in'} flex justify-center items-center h-10 md:h-12`}>
         <img src={clipboard} alt="clipboard" className="w-6 h-6" />
-        <div className="text-center">{this.props.message}</div>
+        <p className="text-center text-sm md:text-base">{this.props.message}</p>
       </div>
     );
   }
