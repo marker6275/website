814408768eab9f72fdbcaffbb3389de9c3b3fd70
Mark Li <mkli627554@gmail.com>
2025-07-26 18:37:29 -0500

    update home project card

--- src/components/cards/HomeProjectCard.js
+++ src/components/cards/HomeProjectCard.js
@@ -26,7 +26,7 @@ export class HomeProjectCard extends React.Component {
               }, 100);
             }
           }}
-          className={`bg-neutral-300 hover:bg-stone-200 font-extralight h-24 lg:h-32 rounded-md flex items-center text-3xl text-left hover:cursor-pointer ${this.props.text} transition-colors transition-all duration-300 p-10 hover:shadow-md`}
+          className={`border-2 border-r-0 border-t-0 hover:border-4 hover:border-r-0 hover:border-t-0 border-neutral-400 hover:border-gray-800 hover:translate-x-2 hover:transition-y-2 font-extralight h-24 lg:h-32 flex items-center text-3xl text-left cursor-pointer ${this.props.text} transition-colors transition-transform duration-300 p-10`}
         >
           {this.props.name}
         </div>
